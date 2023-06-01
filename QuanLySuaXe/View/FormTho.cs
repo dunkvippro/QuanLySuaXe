2a59dcf38bc50563379ce3767092c2709f62b10c
tuananh21225 <tuananh21225@gmail.com>
2023-06-01 19:07:45 +0700

    Update all form

---
+++ QuanLySuaXe/View/FormTho.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySuaXe.View
+{
+    public partial class FormTho : Form
+    {
+        public FormTho()
+        {
+            InitializeComponent();
+        }
+    }
+}
