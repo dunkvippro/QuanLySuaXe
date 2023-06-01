3d3a9c7a765841af9ff48fc0bab4fce7f158c991
tuananh21225 <tuananh21225@gmail.com>
2023-06-01 19:24:45 +0700

    Update Form UserLogin

--- QuanLySuaXe/View/FormUserLogin.Designer.cs
+++ QuanLySuaXe/View/FormUserLogin.Designer.cs
@@ -28,33 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvUserLogin = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Thêm = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbUserID = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.cbbChucVu = new System.Windows.Forms.ComboBox();
+            this.pnlTimKiem = new System.Windows.Forms.Panel();
+            this.cbbTKChucVu = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserLogin)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.pnlTimKiem.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvUserLogin
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(140, 83);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(685, 329);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvUserLogin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUserLogin.Location = new System.Drawing.Point(227, 83);
+            this.dgvUserLogin.Name = "dgvUserLogin";
+            this.dgvUserLogin.Size = new System.Drawing.Size(577, 329);
+            this.dgvUserLogin.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnLuu);
+            this.panel1.Controls.Add(this.btnSua);
+            this.panel1.Controls.Add(this.Thêm);
+            this.panel1.Location = new System.Drawing.Point(292, 420);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 60);
+            this.panel1.TabIndex = 1;
+            // 
+            // Thêm
+            // 
+            this.Thêm.Location = new System.Drawing.Point(113, 15);
+            this.Thêm.Name = "Thêm";
+            this.Thêm.Size = new System.Drawing.Size(90, 30);
+            this.Thêm.TabIndex = 0;
+            this.Thêm.Text = "Thêm";
+            this.Thêm.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(209, 15);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(90, 30);
+            this.btnSua.TabIndex = 0;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(305, 15);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(90, 30);
+            this.btnLuu.TabIndex = 1;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(401, 15);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(90, 30);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.cbbChucVu);
+            this.panel2.Controls.Add(this.txbPassword);
+            this.panel2.Controls.Add(this.txbUserID);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(13, 83);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(208, 329);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chức vụ";
+            // 
+            // txbUserID
+            // 
+            this.txbUserID.Location = new System.Drawing.Point(76, 24);
+            this.txbUserID.Name = "txbUserID";
+            this.txbUserID.Size = new System.Drawing.Size(100, 20);
+            this.txbUserID.TabIndex = 3;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(76, 55);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(100, 20);
+            this.txbPassword.TabIndex = 4;
+            // 
+            // cbbChucVu
+            // 
+            this.cbbChucVu.FormattingEnabled = true;
+            this.cbbChucVu.Location = new System.Drawing.Point(76, 87);
+            this.cbbChucVu.Name = "cbbChucVu";
+            this.cbbChucVu.Size = new System.Drawing.Size(121, 21);
+            this.cbbChucVu.TabIndex = 5;
+            // 
+            // pnlTimKiem
+            // 
+            this.pnlTimKiem.Controls.Add(this.btnTimKiem);
+            this.pnlTimKiem.Controls.Add(this.cbbTKChucVu);
+            this.pnlTimKiem.Controls.Add(this.label4);
+            this.pnlTimKiem.Location = new System.Drawing.Point(227, 13);
+            this.pnlTimKiem.Name = "pnlTimKiem";
+            this.pnlTimKiem.Size = new System.Drawing.Size(577, 64);
+            this.pnlTimKiem.TabIndex = 3;
+            // 
+            // cbbTKChucVu
+            // 
+            this.cbbTKChucVu.FormattingEnabled = true;
+            this.cbbTKChucVu.Location = new System.Drawing.Point(348, 27);
+            this.cbbTKChucVu.Name = "cbbTKChucVu";
+            this.cbbTKChucVu.Size = new System.Drawing.Size(121, 21);
+            this.cbbTKChucVu.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(295, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Chức vụ";
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(475, 18);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(90, 30);
+            this.btnTimKiem.TabIndex = 3;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
             // 
             // FormUserLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 492);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pnlTimKiem);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvUserLogin);
             this.Name = "FormUserLogin";
             this.Text = "FormUserLogin";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserLogin)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.pnlTimKiem.ResumeLayout(false);
+            this.pnlTimKiem.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvUserLogin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button Thêm;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbChucVu;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.TextBox txbUserID;
+        private System.Windows.Forms.Panel pnlTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.ComboBox cbbTKChucVu;
+        private System.Windows.Forms.Label label4;
     }
 }
