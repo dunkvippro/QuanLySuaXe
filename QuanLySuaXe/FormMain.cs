7cd7a96e25f38fadb4ee418143658270505ec9ee
hkdang <duonglatoi111@gmail.com>
2023-06-01 21:03:46 +0700

    9:00pm

--- QuanLySuaXe/FormMain.cs
+++ QuanLySuaXe/FormMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLySuaXe.View;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,35 @@ namespace QuanLySuaXe
         public FormMain()
         {
             InitializeComponent();
+        }
+
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Form curForm;
+        private void OpenChildForm(Form child)
+        {
+            if (this.curForm != null) {
+                this.curForm.Close();
+            }
+
+            curForm = child;
+            child.TopLevel = false;
+            child.FormBorderStyle  = FormBorderStyle.None;
+            child.Dock  =DockStyle.Fill;
+            panel_body.Controls.Add(child);
+            panel_body.Tag  = child;
+            child.BringToFront();
+            child.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new FormUserLogin());
+
         }
     }
 }
